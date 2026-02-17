5b196ad9b0226dfb34ffd3867f45ca46865f85a9
Farid <101619655+faridibirov@users.noreply.github.com>
2026-02-17 17:24:54 +0400

    Creating API and Database Connection

---
+++ RoyalVilla_API/Controllers/VillaController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace RoyalVilla_API.Controllers;
+
+[Route("api/villa")]
+[ApiController]
+public class VillaController : ControllerBase
+{
+
+    [HttpGet]
+    public string GetVillas()
+    {
+        return "Get all Villas";
+    }
+
+    [HttpGet("{id:int}")]
+    public string GetVillaById(int id)
+    {
+        return "Get all Villa: "+id;
+    }
+
+}
---
+++ RoyalVilla_API/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace RoyalVilla_API.Data;
+
+public class ApplicationDbContext(DbContextOptions options) : DbContext(options)
+{
+   public DbSet<Villa> Villas { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        modelBuilder.Entity<Villas>().HasData();
+    }
+}
